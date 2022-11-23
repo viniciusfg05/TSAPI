6bb8befc032e1768d09f3c8ec7036d26b749ef10
unknown <viniciusserlimpa@gmail.com>
2022-11-23 10:27:08 -0300

    Refatorando as rotas

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,21 +1,12 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/implementation/categoriesRepository";
-import { CreateCategoryUseCase } from "../modules/cars/useCases/createCategory/createCategoryUseCase";
 import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationRoutes = Router();
 
-const specificationRepository = new CategoriesRepository();
-
 specificationRoutes.post("/", (req, res) => {
   return createSpecificationController.handle(req, res);
-});
-
-specificationRoutes.get("/", (req, res) => {
-  const all = specificationRepository.list();
-
-  return res.status(201).json(all);
 });
 
 export { specificationRoutes };
