26e0113ef29fcadee2b7fbe7189d05b53176a60b
unknown <viniciusserlimpa@gmail.com>
2022-11-23 11:11:17 -0300

    Upload de arquivos - integrando multe

--- anotações.md
+++ anotações.md
@@ -1,3 +1,5 @@
 ### DTO = Data transfer Object
 
 ### singleton - Padrao para criar instancias unicas para aplicação
+
+### Multer - Upload de arquivos para leitura 
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
+import multer from "multer"
+// import multer from 'multer';
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+
+// eslint-disable-next-line prettier/prettier
+import upload = multer({
+  dest: "./tmp"
+});
 
 const categoriesRoutes = Router();
 
@@ -11,6 +18,12 @@ categoriesRoutes.post("/", (req, res) => {
 
 categoriesRoutes.get("/", (req, res) => {
   return listCategoriesController.handle(req, res);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  const { file } = req
+  console.log(file)
+  return res.send()
 });
 
 export { categoriesRoutes };
